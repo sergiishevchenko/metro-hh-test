acc786a5b149b6dd5808b68b29b38b81721f6b51
Sergii Shevchenko <sergiishevchenko017@gmail.com>
2020-09-30 11:59:37 +0200

    fixed loop

--- metro/views.py
+++ metro/views.py
@@ -3,31 +3,22 @@ from django.shortcuts import render
 from config import access_token
 
 def hh_metro(request):
-    result = requests.get('https://api.hh.ru/metro/4').json()
-    auth = requests.get('https://hh.ru/oauth/authorize?response_type=code&token={}'.format(access_token))
-    result_from_auth = requests.get('https://api.hh.ru/metro/4').json()
+    result = requests.get('https://api.hh.ru/metro/1').json()
+    headers = {'Authorization': 'Bearer {}'.format(access_token), 'User-Agent': 'api-test-agent'}
+    result_from_auth = requests.get('https://api.hh.ru/metro/1', headers=headers, verify=False).json()
 
     new = []
     old = []
     deleted = []
-    for item_auth in result_from_auth['lines']:
-        for item in result['lines']:
-            if len(item['stations']) == len(item_auth['stations']):
-                for k in item['stations']:
-                    old.append(k['name'])
-            else:
-                if len(item['stations']) < len(item_auth['stations']):
-                    mid = []
-                    for w in item_auth['stations']:
-                        mid.append(w['name'])
-                    for j in item['stations']:
-                        if j['name'] not in mid:
-                            new.append(j['name'])
-                else:
-                    mid = []
-                    for w in item_auth['stations']:
-                        mid.append(w['name'])
-                    for j in item['stations']:
-                        if j['name'] not in mid:
-                            deleted.append(j['name'])
+    for line_auth in result_from_auth['lines']:
+        for line in result['lines']:
+            if line['name'] == line_auth['name']:
+                r = []
+                for station in line['stations']:
+                    r.append(station['name'])
+                for station_auth in line_auth['stations']:
+                    if station_auth['name'] in r:
+                        old.append(station_auth['name'])
+                    elif station_auth['name'] not in r:
+                        new.append(station_auth['name'])
     return render(request, 'hh_metro.html', {'new': new, 'old': old, 'deleted': deleted})
